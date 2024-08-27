849d442311c887b65b175a0be971150852e3c61c
HK9750 <hasnainkhan9750@gmail.com>
2024-08-27 22:10:17 +0500

    perfected en variables for production

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -5,20 +5,20 @@ import GoogleProvider from "next-auth/providers/google";
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID ?? "",
-      clientSecret: process.env.GOOGLE_SECRET ?? "",
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_ID ?? "",
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_SECRET ?? "",
     }),
 
     Github({
-      clientId: process.env.GITHUB_ID ?? "",
-      clientSecret: process.env.GITHUB_SECRET ?? "",
+      clientId: process.env.NEXT_PUBLIC_GITHUB_ID ?? "",
+      clientSecret: process.env.NEXT_PUBLIC_GITHUB_SECRET ?? "",
     }),
   ],
   pages: {
     signIn: "/signup",
   },
 
-  secret: process.env.NEXT_AUTH_SECRET,
+  secret: process.env.NEXT_PUBLIC_AUTH_SECRET,
 };
 
 export default authOptions;
