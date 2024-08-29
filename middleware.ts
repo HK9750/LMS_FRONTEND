0f157ec731a7b74d98b1473b434ed020f4567e62
HK9750 <hasnainkhan9750@gmail.com>
2024-08-29 21:59:25 +0500

    added console.log

--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,30 @@
-// import { NextResponse } from "next/server";
-// import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-// export function middleware(request: NextRequest) {
-//   const isRefreshToken = request.cookies.has("refreshToken");
+export function middleware(request: NextRequest) {
+  const isRefreshToken = request.cookies.has("refreshToken");
+  console.log("isRefreshToken", isRefreshToken);
 
-//   const { pathname } = request.nextUrl;
+  const { pathname } = request.nextUrl;
 
-//   const protectedRoutes = [
-//     /^\/profile(\/.*)?$/,
-//     /^\/course(\/.*)?$/,
-//     /^\/dashboard(\/.*)?$/,
-//     /^\/courseaccess(\/.*)?$/,
-//   ];
+  const protectedRoutes = [
+    /^\/profile(\/.*)?$/,
+    /^\/course(\/.*)?$/,
+    /^\/dashboard(\/.*)?$/,
+    /^\/courseaccess(\/.*)?$/,
+  ];
 
-//   const isProtectedRoute = protectedRoutes.some((route) =>
-//     route.test(pathname)
-//   );
+  const isProtectedRoute = protectedRoutes.some((route) =>
+    route.test(pathname)
+  );
 
-//   if (isProtectedRoute && !isRefreshToken) {
-//     const url = request.nextUrl.clone();
-//     url.pathname = "/login";
-//     return NextResponse.redirect(url);
-//   }
+  if (isProtectedRoute && !isRefreshToken) {
+    const url = request.nextUrl.clone();
+    url.pathname = "/login";
+    return NextResponse.redirect(url);
+  }
 
-//   return NextResponse.next();
-// }
+  return NextResponse.next();
+}
 
-// export default middleware;
+export default middleware;
