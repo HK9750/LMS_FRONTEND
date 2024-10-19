9e03c068c21d32492a69cb5d97a47a9b70b59c24
HK9750 <hasnainkhan9750@gmail.com>
2024-10-19 12:57:52 +0500

    /courses suspense error

--- app/(user-routes)/courses/page.tsx
+++ app/(user-routes)/courses/page.tsx
@@ -1,12 +1,22 @@
 "use client";
 import Courses from "@/components/Course/Courses";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
-const Page = () => {
+const CoursesClient = () => {
   const searchParams = useSearchParams();
   const search = searchParams?.get("search");
   console.log(search);
 
   return <Courses search={search} />;
 };
+
+const Page = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <CoursesClient />
+    </Suspense>
+  );
+};
+
 export default Page;
